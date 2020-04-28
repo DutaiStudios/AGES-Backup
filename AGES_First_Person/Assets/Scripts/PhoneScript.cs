eb8dceb6ac6c0d31a5b37445debf86af7510fc44
DutaiStudios <44248998+DutaiStudios@users.noreply.github.com>
2020-04-27 22:09:19 -0400

    Re-Added for Review
    Added new/edited scripts and scene

---
+++ AGES_First_Person/Assets/Scripts/PhoneScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneScript : MonoBehaviour
+{
+
+    [SerializeField] GameObject glow;
+    [SerializeField] GameManager gm;
+    [SerializeField] Animation glowstrobe;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
